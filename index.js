f51c534ee9cf55295759b16f346f7fabb8eee377
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 13:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  var message = `${line.length === 0 ? 'There is nobody waiting to be served!' : `${line[0]}`}`;
+  var message = `${line.length === 0 ? 'There is nobody waiting to be served!' : `Currently serving ${line[0]}`}`;
   line.shift();
   return message
 }
